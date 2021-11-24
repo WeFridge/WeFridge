a1dc9a3bd4b65b8b695968520cf323f932fbd925
Pascal Syma <pascal@syma.dev>
2021-11-24 16:08:01 +0100

    Remove unused override function
    Signed-off-by: Pascal Syma <pascal@syma.dev>

--- app/src/main/java/app/wefridge/wefridge/EditFragment.kt
+++ app/src/main/java/app/wefridge/wefridge/EditFragment.kt
@@ -39,10 +39,6 @@ class EditFragment : Fragment() {
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
     companion object {
         /**
          * Use this factory method to create a new instance of
